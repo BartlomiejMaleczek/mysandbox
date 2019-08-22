a1161a294b3f2f4a9ca2a171f3496055e940f80c
bartlomiej.maleczek <bartlomiej.maleczek@genecloud.pl>
2019-08-22 13:22:57 +0200

    Testing UnsavedChangesCmp

---
+++ src/aura/UnsavedChangesCmp/UnsavedChangesCmpController.js
@@ -0,0 +1,28 @@
+({
+    makeUnsavedChanges: function (cmp, evt, helper) {
+        var unsaved = cmp.find("unsaved");
+        console.log(unsaved);
+        unsaved.setUnsavedChanges(true, {label: 'My component name'});
+    },
+    clearUnsavedChanges: function (cmp, evt, helper) {
+        var unsaved = cmp.find("unsaved");
+        unsaved.setUnsavedChanges(false);
+    },
+    handleSave: function (cmp, evt, helper) {
+        console.log('handleSave');
+        // When the custom save logic has completed the setUnsavedChanges method
+        // must be called again to return control to the lightning UI
+        var unsaved = cmp.find("unsaved");
+        if (true) {
+            // return control to the lightning UI while indicating that the content is still unsaved, preventing it from being dismissed
+            unsaved.setUnsavedChanges(true);
+        } else {
+            // return control to the lightning UI while indicating that the content is saved
+            unsaved.setUnsavedChanges(false);
+        }
+    },
+    handleDiscard: function (cmp, evt, helper) {
+        console.log('handle discard');
+        // similar to the handleSave method, but for discarding changes
+    }
+})
