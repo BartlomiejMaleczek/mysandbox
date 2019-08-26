2c2e50d5f5bc59512d8cdff56a39f9c10cd8e5fb
bartlomiej.maleczek <bartlomiej.maleczek@genecloud.pl>
2019-08-26 17:40:19 +0200

    Refactor code to use object wrapper not many attributes

--- src/aura/ResizableTable/ResizableTableController.js
+++ src/aura/ResizableTable/ResizableTableController.js
@@ -5,47 +5,51 @@
 
     onMouseUp: function (cmp, evt, helper) {
         console.log('onMouseUp');
-        cmp.set("v.isMouseDown", false);
-        cmp.set("v.mousePosition", undefined);
-        cmp.set("v.curColWidth", undefined);
-        cmp.set("v.nxtColWidth", undefined);
+        // cmp.set("v.isMouseDown", false);
+        // cmp.set("v.mousePosition", undefined);
+        // cmp.set("v.curColWidth", undefined);
+        // cmp.set("v.nxtColWidth", undefined);
+
+        cmp.set("v.currentResizableCol", undefined);
     },
 
     onMouseMove: function (cmp, evt, helper) {
         // window.setTimeout(function() {
-                if (cmp.get("v.isMouseDown")) {
-                    var currCol = cmp.get("v.curCol");
-                    var nextCol = cmp.get("v.nxtCol");
+        var currCol = cmp.get("v.currentResizableCol");
+                if (currCol) {
+
+                    // var nextCol = cmp.get("v.nxtCol");
 
                     var currLastDifX = cmp.get("v.currLastDifX");
-                    var nextLastDifX = cmp.get("v.nextLastDifX");
+                    // var nextLastDifX = cmp.get("v.nextLastDifX");
 
-                    var diffX = evt.pageX - cmp.get("v.mousePosition");
+                    // var diffX = evt.pageX - cmp.get("v.mousePosition");
+                    var diffX = currCol.calculateDiffWithMousePos(evt.pageX);
+                    var diffCurrCol = currCol.calculateDiffCurrCol(diffX);
 
-                    var diffCurrCol = (cmp.get("v.curColWidth") + diffX);
-                    var diffNextCol = (cmp.get("v.nxtColWidth") - diffX);
+                    // var diffCurrCol = (cmp.get("v.curColWidth") + diffX);
+                    // var diffNextCol = (cmp.get("v.nxtColWidth") - diffX);
 
-                    if (nextCol) {
-                        if ((currCol.offsetWidth > helper.CONSTANTS.MIN_COL_SIZE || currLastDifX < diffX)) {
-                            if (currCol && currCol.style) {
-                                currCol.style.width = diffCurrCol + 'px';
+                    // if (nextCol) {
+                        if ((currCol.currElem.offsetWidth > helper.CONSTANTS.MIN_COL_SIZE || currLastDifX < diffX)) {
+                            if (currCol.currElem && currCol.currElem.style) {
+                                currCol.currElem.style.width = diffCurrCol + 'px';
                             }
-
                             cmp.set("v.currLastDifX", undefined);
                         } else {
                             cmp.set("v.currLastDifX", diffX);
                         }
 
-                        if ((nextCol.offsetWidth > helper.CONSTANTS.MIN_COL_SIZE || nextLastDifX > diffX)) {
-                            if (nextCol.style) {
-                                nextCol.style.width = diffNextCol + 'px';
-                            }
-
-                            cmp.set("v.nextLastDifX", undefined);
-                        } else {
-                            cmp.set("v.nextLastDifX", diffX);
-                        }
-                    }
+                        // if ((nextCol.offsetWidth > helper.CONSTANTS.MIN_COL_SIZE || nextLastDifX > diffX)) {
+                        //     if (nextCol.style) {
+                        //         nextCol.style.width = diffNextCol + 'px';
+                        //     }
+                        //
+                        //     cmp.set("v.nextLastDifX", undefined);
+                        // } else {
+                        //     cmp.set("v.nextLastDifX", diffX);
+                        // }
+                    // }
                 }
         // }
         //     ,0);
@@ -53,35 +57,37 @@
     },
 
     onMouseDown: function (cmp, evt, helper) {
-        var curCol = evt.target.parentElement.parentElement;
-        var nxtCol = curCol.nextElementSibling;
-        var pageX = evt.pageX;
+        var currentResizableCol = new helper.CurrentResizableCol(evt);
+        cmp.set("v.currentResizableCol", currentResizableCol);
 
-        console.log(curCol);
-
-        if(!cmp.get("v.defaultColWidth")) {
-            cmp.set("v.defaultColWidth", curCol.offsetWidth);
-        }
-
-        cmp.set("v.mousePosition", pageX);
-        cmp.set("v.curColWidth", curCol.offsetWidth);
-        cmp.set("v.isMouseDown", true);
-        cmp.set("v.curCol", curCol);
-        if (nxtCol) {
-            cmp.set("v.nxtColWidth", nxtCol.offsetWidth);
-            cmp.set("v.nxtCol", nxtCol);
-        }
+        // var curCol = evt.target.parentElement.parentElement;
+        // var nxtCol = curCol.nextElementSibling;
+        // var pageX = evt.pageX;
+        //
+        // if(!cmp.get("v.defaultColWidth")) {
+        //     cmp.set("v.defaultColWidth", curCol.offsetWidth);
+        // }
+        //
+        // cmp.set("v.mousePosition", pageX);
+        // cmp.set("v.curColWidth", curCol.offsetWidth);
+        // cmp.set("v.isMouseDown", true);
+        // cmp.set("v.curCol", curCol);
+        // if (nxtCol) {
+        //     cmp.set("v.nxtColWidth", nxtCol.offsetWidth);
+        //     cmp.set("v.nxtCol", nxtCol);
+        // }
 
         if(evt.stopPropagation) evt.stopPropagation();
         if(evt.preventDefault) evt.preventDefault();
     },
 
     handleResetWidth: function (cmp, evt, helper) {
-        var defaultColWidth = cmp.get("v.defaultColWidth");
-        if(defaultColWidth) {
+        // var defaultColWidth = cmp.get("v.defaultColWidth");
+        // if(defaultColWidth) {
             document.querySelectorAll(".reset-width").forEach(function (trElem) {
-                trElem.style.width = defaultColWidth + 'px';
+                console.log(helper.CONSTANTS.DEFAULT_COL_SIZE);
+                trElem.style.width = helper.CONSTANTS.DEFAULT_COL_SIZE + 'px';
             });
-        }
+        // }
     }
 })
--- src/aura/ResizableTable/ResizableTableHelper.js
+++ src/aura/ResizableTable/ResizableTableHelper.js
@@ -1,5 +1,23 @@
 ({
     CONSTANTS: {
-        MIN_COL_SIZE: 40
+        MIN_COL_SIZE: 50,
+        DEFAULT_COL_SIZE: 150
+    },
+
+    CurrentResizableCol: function (evt) {
+        this.currElem = evt.target.parentElement.parentElement;
+        this.nextElem = this.currElem.nextElementSibling;
+        this.mousePosition = evt.pageX;
+        this.currElemWidthBeforeResize = this.currElem.offsetWidth;
+
+        this.calculateDiffWithMousePos = function (pageX) {
+            return pageX - this.mousePosition;
+        };
+
+        this.calculateDiffCurrCol = function (diffWithMousePos) {
+            return this.currElemWidthBeforeResize + diffWithMousePos;
+        };
+
     }
+
 })
