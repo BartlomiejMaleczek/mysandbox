de13ba073df148b575a2dff55a7db6fe6afe55b9
bartlomiej.maleczek <bartlomiej.maleczek@genecloud.pl>
2019-08-27 13:03:52 +0200

    add MouseUp to logic

--- src/aura/ResizableTable/ResizableTableController.js
+++ src/aura/ResizableTable/ResizableTableController.js
@@ -6,27 +6,30 @@
     },
 
     onMouseUp: function (cmp, evt, helper) {
-        var currentResizableCol = cmp.get("v.currentResizableCol");
-        if(currentResizableCol) {
-            var newWidth = currentResizableCol.resizeableDivider.style.width;
-            var newWidthSubStr = newWidth.substring(0, newWidth.length - 2);
+        cmp.set("v.isMouseUp", true);
+        window.setTimeout(function(){
+            var currentResizableCol = cmp.get("v.currentResizableCol");
+            if(currentResizableCol) {
+                var newWidth = currentResizableCol.resizeableDivider.style.width;
+                var newWidthSubStr = newWidth.substring(0, newWidth.length - 2);
 
-            if(newWidthSubStr >= 50) {
-                currentResizableCol.resizableCol.style.width = newWidth;
-            } else {
-                currentResizableCol.resizableCol.style.width = helper.CONSTANTS.MIN_COL_SIZE + 'px';
-                currentResizableCol.resizeableDivider.style.width = helper.CONSTANTS.MIN_COL_SIZE + 'px';
+                if(newWidthSubStr >= 50) {
+                    currentResizableCol.resizableCol.style.width = newWidth;
+                } else {
+                    currentResizableCol.resizableCol.style.width = helper.CONSTANTS.MIN_COL_SIZE + 'px';
+                    currentResizableCol.resizeableDivider.style.width = helper.CONSTANTS.MIN_COL_SIZE + 'px';
+                }
             }
-
             cmp.set("v.currentResizableCol", undefined);
+            }, 200);
 
-        }
 
     },
 
     onMouseMove: function (cmp, evt, helper) {
         var currentResizableCol = cmp.get("v.currentResizableCol");
-        if (currentResizableCol) {
+        var isMouseUp = cmp.get("v.isMouseUp");
+        if (currentResizableCol && !isMouseUp) {
             var newWidth = currentResizableCol.calculateNewWidth(evt.pageX);
             currentResizableCol.resizeableDivider.style.width = newWidth + 'px';
         }
@@ -35,7 +38,7 @@
     onMouseDown: function (cmp, evt, helper) {
         var currentResizableCol = new helper.CurrentResizableCol(evt);
         cmp.set("v.currentResizableCol", currentResizableCol);
-
+        cmp.set("v.isMouseUp", false);
         if (evt.stopPropagation) evt.stopPropagation();
         if (evt.preventDefault) evt.preventDefault();
     },
