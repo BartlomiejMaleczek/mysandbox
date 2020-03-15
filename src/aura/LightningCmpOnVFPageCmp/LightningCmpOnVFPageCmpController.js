c32f61bb275532aa4aeaff8b7f61ac33cabec788
bartlomiej.maleczek <bartlomiej.maleczek@genecloud.pl>
2020-03-15 16:38:20 +0100

    test vf page with script

--- src/aura/LightningCmpOnVFPageCmp/LightningCmpOnVFPageCmpController.js
+++ src/aura/LightningCmpOnVFPageCmp/LightningCmpOnVFPageCmpController.js
@@ -4,19 +4,36 @@
 
     },
 
-    handleNavigate: function (cmp, evt, helper) {
-        console.log('handleNavigate');
-        var navService = cmp.find("navService");
-        console.log(navService);
+    handleShowNotice : function(component, event, helper) {
 
-        var pageReference = {
-            type: 'standard__recordPage',
-            attributes: {
-                actionName: 'view',
-                objectApiName: 'Account',
-                recordId: '0011n000024jUpdAAE'
-            },
-        };
+        // console.log(component.find('notifLib'));
+        //
+        // component.find('notifLib').showToast({
+        //     "title": "Success!",
+        //     "message": "Record {0} created! See it {1}!",
+        //     "messageData": [
+        //         'Salesforce',
+        //         {
+        //             url: 'http://www.salesforce.com/',
+        //             label: 'here',
+        //         }
+        //     ]
+        // });
+    },
+
+    handleNavigate: function (cmp, evt, helper) {
+        // console.log('handleNavigate');
+        // var navService = cmp.find("navService");
+        // console.log(navService);
+        //
+        // var pageReference = {
+        //     type: 'standard__recordPage',
+        //     attributes: {
+        //         actionName: 'view',
+        //         objectApiName: 'Account',
+        //         recordId: '0011n000024jUpdAAE'
+        //     },
+        // };
 
         // let pageReference = {
         //     type: 'standard__webPage',
@@ -43,19 +60,19 @@
 
         // debugger;
 
-        console.log(helper.isLightningDesktop());
+        // console.log(helper.isLightningDesktop());
         //
         // debugger;
         //
-        navService.generateUrl(pageReference)
-            .then($A.getCallback(function (url) {
-                console.log('URL', url);
-                // navService.navigate(pageReference);
-
-
-            }), $A.getCallback(function (error) {
-                console.log(error);
-            }));
+        // navService.generateUrl(pageReference)
+        //     .then($A.getCallback(function (url) {
+        //         console.log('URL', url);
+        //         // navService.navigate(pageReference);
+        //
+        //
+        //     }), $A.getCallback(function (error) {
+        //         console.log(error);
+        //     }));
 
 
     }
