c4aca17e6eb9d769bba3f916e348773db42c0196
bartlomiej.maleczek <bartlomiej.maleczek@genecloud.pl>
2020-07-03 17:56:33 +0200

    community-poc - finish styling icons nav

--- force-app/main/default/lwc/navHeader/navHeader.js
+++ force-app/main/default/lwc/navHeader/navHeader.js
@@ -34,12 +34,14 @@ export default class NavHeader extends LightningElement {
 
     linkMouseOverTimeout = null;
     dropdownMouseOverTimeout = null;
+    dropdownIconsMouseOverTimeout = null;
 
     expandableContent = [];
     currentExpandableNavItemName;
     isRendered = false;
 
     dropdownContent = {};
+    activeNavIconId;
 
     constructor() {
         super();
@@ -276,6 +278,67 @@ export default class NavHeader extends LightningElement {
             contentElem.setAttribute('opacity', 1);
         }.bind(this), 500);
 
+    }
+
+    handleMouseOverNavIcon(event) {
+        clearTimeout(this.dropdownIconsMouseOverTimeout);
+
+        this.activeNavIconId = event.currentTarget.getAttribute('data-nav-icon-type');
+
+        this.openNavIconDropdown();
+    }
+
+    handleMouseLeaveNavIcon() {
+        this.dropdownIconsMouseOverTimeout = setTimeout(function () {
+            this.closeNavIconDropdown();
+        }.bind(this), 200);
+    }
+
+    openNavIconDropdown() {
+        const navIconDropdown = this.template.querySelector('.nav-items-icons-group-dropdown');
+        navIconDropdown.style.setProperty('opacity', '1');
+    }
+
+    closeNavIconDropdown() {
+        const navIconDropdown = this.template.querySelector('.nav-items-icons-group-dropdown');
+        navIconDropdown.style.setProperty('opacity', '0');
+    }
+
+    activateNavIcon() {
+        // const navIcon = this.template.querySelector('div[data-nav-icon-type=' + this.activeNavIconId + ']');
+        // console.log('navIcon', navIcon);
+        // navIcon.classList.add('nav-item-icon-is-active');
+    }
+
+    deactivateNavIcon() {
+        // const navIcon = this.template.querySelector('.nav-item-icon-is-active');
+        //
+        // if(navIcon) {
+        //     navIcon.classList.remove('nav-item-icon-is-active');
+        // }
+    }
+
+    handleMouseOverDropdownIcons() {
+        clearTimeout(this.dropdownIconsMouseOverTimeout);
+
+        // this.activateNavIcon();
+    }
+
+    handleMouseLeaveDropdownIcons() {
+        this.closeNavIconDropdown();
+        // this.deactivateNavIcon();
+    }
+
+    get isProfileIconActive() {
+        return this.activeNavIconId == 'profile';
+    }
+
+    get isOrderIconActive() {
+        return this.activeNavIconId == 'orders';
+    }
+
+    get isResourcesIconActive() {
+        return this.activeNavIconId == 'resources';
     }
 
 
