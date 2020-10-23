1ca4ca2159a109dc76a2a659d514a0e6ca43435f
bartlomiej.maleczek <bartlomiej.maleczek@genecloud.pl>
2020-10-23 13:10:58 +0200

    poc com

--- force-app/main/default/lwc/lwcPOC/lwcPOC.js
+++ force-app/main/default/lwc/lwcPOC/lwcPOC.js
@@ -5,5 +5,14 @@
 import {LightningElement} from 'lwc';
 
 export default class LwcPoc extends LightningElement {
-
+    renderedCallback() {
+        const style = document.createElement('style');
+        style.innerText = `.slds-accordion__section {background-color: red;padding: 0.5rem;}`;
+        var accordionSection = this.template.querySelector('lightning-accordion-section');
+        console.log('accordionSection', accordionSection);
+        if (accordionSection) {
+            console.log('append child', style);
+            accordionSection.appendChild(style);
+        };
+    }
 }
